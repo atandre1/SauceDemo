5285b920f2074860489b4fbfe8b2d36a73c727d1
atandre1 <atandrei@protonmail.com>
2022-08-18 19:46:57 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod(alwaysRun = true)  // запускать всегда
+    public void close() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/ProductTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class ProductTest extends BaseTest{
+
+    @Test
+    public void buyProduct() {
+        driver.get("");
+    }
+}
